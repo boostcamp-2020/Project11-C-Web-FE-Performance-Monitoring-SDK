38bdea9030e4cebb3cf8b30e7a2bbb714728cd73
Dongmin Shin <nsdm821@gmail.com>
2020-11-25 00:04:55 +0900

    feat:  frontend 환경에서 error 수집 및 전송하는 함수

--- src/BrowserErrorHandler.ts
+++ src/BrowserErrorHandler.ts
@@ -1,16 +1,44 @@
 // 브라우저에서 동작하는 js 에러 전부 캡쳐
-window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
-  alert(
-    'Error: ' +
-      errorMsg +
-      ' Script: ' +
-      url +
-      ' Line: ' +
-      lineNumber +
-      ' Column: ' +
-      column +
-      ' StackTrace: ' +
-      errorObj +
-      errorObj.stack
-  );
-};
+import sendLog from './HttpTransport';
+
+export default function setBroswerErrorCaptureHandler() {
+  console.log('start capturing error');
+
+  window.onunhandledrejection = function (e) {
+    console.log(e.reason);
+
+    const data = {
+      content: e.reason.message + e.reason.stack,
+      date: new Date(),
+      env: {},
+    };
+    console.log(data);
+    sendLog(data).then(res => {
+      console.log(res);
+    });
+  };
+
+  window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
+    const data = {
+      content:
+        'Error: ' +
+        errorMsg +
+        ' Script: ' +
+        url +
+        ' Line: ' +
+        lineNumber +
+        ' Column: ' +
+        column +
+        ' StackTrace: ' +
+        errorObj +
+        errorObj.stack,
+      date: new Date(),
+      env: {},
+    };
+
+    console.log(data);
+    sendLog(data).then(res => {
+      console.log(res);
+    });
+  };
+}
