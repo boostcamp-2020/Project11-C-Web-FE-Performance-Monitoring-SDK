a6ec368c141bb2549c7947dc07c7b674791e2bb7
ohmink <kkop5056@naver.com>
2020-12-01 18:34:15 +0900

    fix: 변수 수정, 에러 코드 추적 삭제 서버에 보내는 데이터를 수정하고, 에러 코드를 추적 가능한가에 대한 테스트 진행 후 해당 코드 삭제

--- browser/src/tracer.ts
+++ browser/src/tracer.ts
@@ -8,34 +8,23 @@ const startErrorCapturing = () => {
       content: `name: ${`${event.reason.name}`}   \n  errmsg:${
         event.reason.message
       } \n stackmsg:${event.reason.stack} `,
-      errArea: { none: 'none' },
-      userInfo: { none: 'none' },
+      errArea: {},
+      userInfo: {},
       date: new Date(),
     };
 
-    const result: any = await transport.sendLog(data);
+    const errorId: string | boolean = await transport.sendLog(data);
+    console.log(errorId);
   };
 
   window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
     const data = {
-      content:
-        'Error: ' +
-        errorMsg +
-        ' Script: ' +
-        url +
-        ' Line: ' +
-        lineNumber +
-        ' Column: ' +
-        column +
-        ' StackTrace: ' +
-        errorObj +
-        errorObj.stack,
-      errArea: { none: 'none' },
-      userInfo: { none: 'none' },
+      content: `Error: ${errorMsg}\nScript: ${url}\nLine: ${lineNumber}\nColumn: ${column}\nStack trace: ${errorObj}\n${errorObj.stack}`,
+      errArea: {},
+      userInfo: {},
       date: new Date(),
     };
 
-    console.log(data);
     transport.sendLog(data).then(res => {
       console.log(res);
     });
