778ba66923ce0da2af43b63f4b307728df4bc1b1
ohmink <kkop5056@naver.com>
2020-12-07 22:14:06 +0900

    refactor: 인터페이스 추가 any타입을 대신하여 인터페이스 추가

--- core/src/transport.ts
+++ core/src/transport.ts
@@ -1,13 +1,42 @@
 import axios from 'axios';
 import * as pocket from './pocket';
 
-const sendLog = async (data: { content: string; errArea: {}; date: Date }) => {
+interface LogData {
+  name: string;
+  message: string;
+  stack: string;
+  errArea: {};
+  tags: {};
+  date: number;
+}
+
+interface ResType {
+  config: {};
+  data: {
+    comments: string[];
+    createdAt: string;
+    description: string;
+    errorEvents: {}[];
+    groupHash: string;
+    title: string;
+    updatedAt: string;
+    __v: number;
+    _id: string;
+  };
+  headers: {};
+  request: XMLHttpRequest;
+  status: number;
+  statusText: string;
+}
+
+const sendLog = async (data: LogData) => {
   try {
     const endpoint = pocket.initOptions.dsn;
-    const response: any = await axios.post(endpoint, data);
+    const response: ResType = await axios.post(endpoint, data);
     const errorId: string = response.data._id;
     return errorId;
   } catch (error) {
+    // 생각
     return false;
   }
 };
